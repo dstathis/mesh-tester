efb6f5f751cfb95208bdc38ff59b1cffb9a3d27e
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2024-10-11 01:50:59 +0000

    update lib

--- lib/charms/istio_beacon_k8s/v0/service_mesh.py
+++ lib/charms/istio_beacon_k8s/v0/service_mesh.py
@@ -8,61 +8,97 @@ The library leverages the `service_mesh` and `cross_model_mesh` interfaces.
 
 ##Consumer
 
-To add service mesh support to your charm, instantiate a ServiceMeshConsumer object in the
+Service meshes provide a range of capabilities for routing, controlling, and monitoring traffic.  A key feature of many service meshes is the ability to restrict traffic between Pods based on L4 and L7 criteria.  For example, defining that a Pod MetricsScraper can `GET` from Pod MetricsProducer at `/metrics` on port `9090`, but SomeOtherPod cannot.
+
+The ServiceMeshConsumer object is used to subscribe a charm and its workloads to a related service mesh.  Since it is common for a relation between applications to indicate traffic flow (ex: if DbConsumer Requires a DbProducer), the ServiceMeshConsumer provides an optional way to automate creation of traffic rules based on app relations.
+
+To add service mesh support to your charm, you must add 3 relations in your charmcraft.yaml.
+
+```
+requires:
+  service-mesh:
+    limit: 1
+    interface: service_mesh
+  require-cmr-mesh:
+    interface: cross_model_mesh
+provides:
+  provide-cmr-mesh:
+    interface: cross_model_mesh
+```
+
+Then instantiate a ServiceMeshConsumer object in the
 `__init__` method of your charm:
 
 ```
 from charms.istio_beacon_k8s.v0.service_mesh import Policy, ServiceMeshConsumer
 
 ...
-try:
-    self._mesh = ServiceMeshConsumer(
-        self,
-        policies=[
-            Policy(
-                relation="logging",
-                endpoints=[
-                    Endpoint(
-                        hosts=[self._my_host_name],
-                        ports=[HTTP_LISTEN_PORT],
-                        methods=["GET"],
-                        paths=["/foo"],
-                    ),
-                ],
-                service=self._my_k8s_service(),
-            ),
-        ],
-    )
-except ops.TooManyRelatedAppsError as e:
-    self.unit.status = BlockedStatus(e)
+self._mesh = ServiceMeshConsumer(
+    self,
+    policies=[
+        Policy(
+            relation="metrics",
+            endpoints=[
+                Endpoint(
+                    hosts=[self._my_host_name],
+                    ports=[HTTP_LISTEN_PORT],
+                    methods=["GET"],
+                    paths=["/metrics"],
+                ),
+            ],
+        ),
+        Policy(
+            relation="data",
+            endpoint=[
+                Endpoint(
+                    hosts=[self._my_host_name],
+                    ports[HTTP_LISTEN_PORT],
+                    methods=["GET"]
+                    paths=["/data"],
+                ),
+            ],
+        ),
+    ],
+)
 ```
 
-You will then receive the labels which you need to add your product to the mesh:
-```
-def _on_mesh_relation_changed(self, event):
-    self._apply_labels to pods(self._mesh.labels())
+The example above specifies two policies. The resulting behaviour would be that when related over the `metrics` relation, the service mesh will allow traffic to the `/metrics` endpoint for the remote application and when related over the `data` endpoint, the service mesh will allow traffic to the `/data` endpoint.
+
+By using the above method, you can specify exactly which endpoints can be reached over which relations.
+
+###Cross Model relations
+
+If a ServiceMeshConsumer is creating a Policy for a relation that is cross model, additional information is required to construct the policy. To facilitate this, the charms can also be related over the cross_model_mesh interface. When that relation is established, traffic will be allowed from the requirer to the provider.
+
+###Joining the Mesh
+
+For most charms, simply instantiating ServiceMeshConsumer should automatically configure the charm to be on the mesh. If your charm is the one of the old "podspec" style charms or your charm deploys custom k8s resources there is an addition step. You must apply the labels returned by `ServiceMeshConsumer.labels()` to your pods.
 
 ##Provider
 
 To provide a service mesh, instantiate the ServiceMeshProvider object in the __init__ method
 of your charm:
 ```
-from charms.istio_beacon_k8s.v0.service_mesh import Policy, ServiceMeshProvider
+from charms.istio_beacon_k8s.v0.service_mesh import ServiceMeshProvider
 
 ...
 self._mesh = ServiceMeshProvider(
     charm = self,
-    labels = self._get_mesh_labels(),
+    labels = {"my_service_mesh": "enable"},
     mesh_relation_name = "service-mesh",
 )
 ```
 
-The labels argument should be a dict containing the Kubernetes labels which the client charm needs
-to apply to join the mesh.
+The labels argument is the dict of labels that indicate to the service mesh that a Pod should be subscribed to the mesh.
+These will be sent to each related ServiceMeshConsumer to be used by the charm for per-Pod subscription.  These labels
+are service-mesh dependent - for example, for an Istio ambient mesh this should be
+{"istio.io/dataplane-mode": "ambient"}.
 
-You can then use the relation data to build your authorization policies:
+The provider also exposes the method `mesh_info` that returns a list of MeshPolicy objects. These MeshPolicy objects can be used to configure the service mesh.
+
 ```
-self._build_authorization_policies(self._mesh.mesh_info())
+for policy in self._mesh.mesh_info():
+    set_up_my_mesh(policy)
 ```
 """
 
@@ -72,13 +108,18 @@ import logging
 from typing import Dict, List, Optional
 
 import pydantic
+from lightkube.core.client import Client
+from lightkube.core.exceptions import ApiError as LightkubeApiError
+from lightkube.models.meta_v1 import ObjectMeta
+from lightkube.resources.apps_v1 import StatefulSet
+from lightkube.resources.core_v1 import ConfigMap
 from ops import CharmBase, Object, Relation
 
 LIBID = "3f40cb7e3569454a92ac2541c5ca0a0c"  # Never change this
 LIBAPI = 0
 LIBPATCH = 1
 
-PYDEPS = ["pydantic"]
+PYDEPS = ["lightkube", "pydantic"]
 
 logger = logging.getLogger(__name__)
 
@@ -142,6 +183,7 @@ class ServiceMeshConsumer(Object):
         cross_model_mesh_requires_name: str = "require-cmr-mesh",
         cross_model_mesh_provides_name: str = "provide-cmr-mesh",
         policies: Optional[List[Policy]] = None,
+        auto_join: bool = True,
     ):
         """Class used for joining a service mesh.
 
@@ -154,25 +196,39 @@ class ServiceMeshConsumer(Object):
             cross_model_mesh_provides_name: The relation name as defined in metadata.yaml or
                 charmcraft.yaml for the relation which provides the cross_model_mesh interface.
             policies: List of access policies this charm supports.
+            auto_join: Automatically join the mesh by applying labels to charm pods.
         """
         super().__init__(charm, mesh_relation_name)
         self._charm = charm
         self._relation = self._charm.model.get_relation(mesh_relation_name)
         self._cmr_relations = self._charm.model.relations[cross_model_mesh_provides_name]
         self._policies = policies or []
+        self._label_configmap_name = f"juju-service-mesh-{self._charm.app.name}-labels"
+        if auto_join:
+            self.framework.observe(
+                self._charm.on[mesh_relation_name].relation_changed, self._update_labels
+            )
+            self.framework.observe(
+                self._charm.on[mesh_relation_name].relation_broken, self._on_mesh_broken
+            )
         self.framework.observe(
             self._charm.on[mesh_relation_name].relation_created, self._relations_changed
         )
         self.framework.observe(
             self._charm.on[cross_model_mesh_requires_name].relation_created, self._send_cmr_data
         )
+        self.framework.observe(
+            self._charm.on[cross_model_mesh_provides_name].relation_changed,
+            self._relations_changed,
+        )
         self.framework.observe(self._charm.on.upgrade_charm, self._relations_changed)
-        for policy in self._policies:
+        relations = {policy.relation for policy in self._policies}
+        for relation in relations:
             self.framework.observe(
-                self._charm.on[policy.relation].relation_created, self._relations_changed
+                self._charm.on[relation].relation_created, self._relations_changed
             )
             self.framework.observe(
-                self._charm.on[policy.relation].relation_broken, self._relations_changed
+                self._charm.on[relation].relation_broken, self._relations_changed
             )
 
     def _send_cmr_data(self, event):
@@ -199,7 +255,7 @@ class ServiceMeshConsumer(Object):
             for relation in self._charm.model.relations[policy.relation]:
                 if cmr_relation := self._check_cmr(relation):
                     logger.debug(f"Found cross model relation: {relation.name}. Creating policy.")
-                    cmr_data = CMRData.parse_obj(
+                    cmr_data = CMRData.model_validate(
                         json.loads(cmr_relation.data[cmr_relation.app]["cmr_data"])
                     )
                     mesh_policies.append(
@@ -227,11 +283,20 @@ class ServiceMeshConsumer(Object):
         self._relation.data[self._charm.app]["policies"] = json.dumps(mesh_policies)
 
     def _check_cmr(self, mesh_relation: Relation) -> Optional[Relation]:
+        """Check if the given relation is a cmr. If so return the associated cross_model_mesh relation.
+
+        Returns:
+            Return an instance of the cmr relation if it is found and has data. If there is no
+            data or the relation does not exist, return None.
+        """
         for cmr_rel in self._cmr_relations:
+            # These are the app names as seen by the consumer and are local to the model. When
+            # establishing cross-model relations, it is not possible to represent the app with an
+            # already existing name. Thus these names will always be unique.
             if cmr_rel.app.name == mesh_relation.app.name:
                 if "cmr_data" not in cmr_rel.data[cmr_rel.app]:
                     # Data has not yet been provided
-                    continue
+                    return None
                 return cmr_rel
         return None
 
@@ -241,11 +306,59 @@ class ServiceMeshConsumer(Object):
         # should consider if there is a better way to do this.
         return self._charm.model.name
 
-    def labels(self):
+    def labels(self) -> dict:
         """Labels required for a pod to join the mesh."""
-        if self._relation is None:
+        if self._relation is None or "labels" not in self._relation.data[self._relation.app]:
             return {}
         return json.loads(self._relation.data[self._relation.app]["labels"])
+
+    def _on_mesh_broken(self, _event):
+        self._set_labels({})
+        self._delete_label_configmap()
+
+    def _update_labels(self, _event):
+        self._set_labels(self.labels())
+
+    def _set_labels(self, labels: dict) -> None:
+        client = Client(namespace=self._charm.model.name, field_manager=self._charm.app.name)
+        stateful_set = client.get(res=StatefulSet, name=self._charm.app.name)
+        try:
+            config_map = client.get(ConfigMap, self._label_configmap_name)
+        except LightkubeApiError as e:
+            if "not found" in str(e):
+                config_map = self._create_label_configmap(client)
+            else:
+                raise
+        if config_map.data:
+            config_map_labels = json.loads(config_map.data["labels"])
+            for label in config_map_labels:
+                if label not in labels:
+                    # The label was previously set. Setting it to None will delete it.
+                    labels[label] = None
+        if stateful_set.spec:
+            stateful_set.spec.template.metadata.labels.update(labels)
+        config_map.data = {"labels": json.dumps({key: value for (key, value) in labels.items() if value is not None})}
+        # TODO: We should look into whether it is possible to do the next two commands as one
+        # transaction. As it is now, if the code crashes between the two commands, we could end up
+        # leaving dangling labels on the StatefulSet.
+        client.patch(res=ConfigMap, name=self._label_configmap_name, obj=config_map)
+        client.patch(res=StatefulSet, name=self._charm.app.name, obj=stateful_set)
+
+    def _create_label_configmap(self, client) -> ConfigMap:
+        """Create an empty ConfigMap unique to this charm."""
+        obj = ConfigMap(
+                data={"labels": "{}"},
+            metadata=ObjectMeta(
+                name=self._label_configmap_name,
+                namespace=self._charm.model.name,
+            ),
+        )
+        client.create(obj=obj)
+        return obj
+
+    def _delete_label_configmap(self) -> None:
+        client = Client(namespace=self._charm.model.name, field_manager=self._charm.app.name)
+        clinet.delete(res=ConfigMap, name=self._label_configmap_name)
 
 
 class ServiceMeshProvider(Object):
@@ -279,11 +392,11 @@ class ServiceMeshProvider(Object):
         for relation in self._charm.model.relations[self._relation_name]:
             relation.data[self._charm.app]["labels"] = rel_data
 
-    def mesh_info(self):
-        """Return the relation data used to define authorization policies on the mesh."""
+    def mesh_info(self) -> List[MeshPolicy]:
+        """Return the relation data that defines Policies requested by the related applications."""
         mesh_info = []
         for relation in self._charm.model.relations[self._relation_name]:
-            rel_data = json.loads(relation.data[relation.app]["mesh_data"])
-            rel_data["policies"] = [Policy.parse_obj(p) for p in rel_data["policies"]]
-            mesh_info.append(rel_data)
+            policies_data = json.loads(relation.data[relation.app]["policies"])
+            policies = [MeshPolicy.model_validate(policy) for policy in policies_data]
+            mesh_info.extend(policies)
         return mesh_info
